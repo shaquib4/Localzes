d2277d756bd950c9bc49001d1bb647481dda1a4f
Shaquib <mohammadshaquib4@gmail.com>
2020-10-26 20:05:08 +0530

    other files added

---
+++ app/src/main/java/com/example/localzes/SellerShop_detail.kt
@@ -0,0 +1,11 @@
+package com.example.localzes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerShop_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_shop_detail)
+    }
+}
