3daff7adc932a44fa0e368e8a6acdf43ce5aada0
Shaquib <mohammadshaquib4@gmail.com>
2020-10-26 17:39:39 +0530

    home to continuesas

---
+++ app/src/main/java/com/example/localzes/Registerdetails_seller.kt
@@ -0,0 +1,11 @@
+package com.example.localzes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registerdetails_seller : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registerdetails_seller)
+    }
+}
