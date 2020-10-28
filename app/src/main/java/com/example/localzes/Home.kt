c0ec0b88741a535e41b3f54c525b2bdb8e83e79d
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-10-28 16:43:48 +0530

    changes made

--- app/src/main/java/com/example/localzes/Home.kt
+++ app/src/main/java/com/example/localzes/Home.kt
@@ -11,53 +11,75 @@ import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
-    var userDatabase: DatabaseReference?=null
-    var firebaseUser: FirebaseUser?=null
+    private lateinit var userDatabase: DatabaseReference
+    var firebaseUser: FirebaseUser? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        progress_home.visibility= View.VISIBLE
+        progress_home.visibility = View.VISIBLE
 
 
 
-        firebaseUser= FirebaseAuth.getInstance().currentUser
-        userDatabase= FirebaseDatabase.getInstance().reference.child("seller").child(firebaseUser!!.uid)
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        userDatabase = FirebaseDatabase.getInstance().reference.child("seller")
 
 
         bottom_navHome.selectedItemId = R.id.nav_home
         bottom_navHome.setOnNavigationItemSelectedListener { item ->
 
 
-            when(item.itemId){
+            when (item.itemId) {
 
-                R.id.nav_home->{
+                R.id.nav_home -> {
 
                     return@setOnNavigationItemSelectedListener true
                 }
-               R.id.nav_search->{
+                R.id.nav_search -> {
 
                     startActivity(Intent(this, Search::class.java))
-                    overridePendingTransition(0,0)
+                    overridePendingTransition(0, 0)
                     finish()
 
                 }
-                R.id.nav_cart->{
+                R.id.nav_cart -> {
 
-                    startActivity(Intent(this,Cart::class.java))
-                    overridePendingTransition(0,0)
+                    startActivity(Intent(this, Cart::class.java))
+                    overridePendingTransition(0, 0)
                     finish()
 
                 }
-               R.id.nav_account->{
+                R.id.nav_account -> {
 
                     startActivity(Intent(this, Accounts::class.java))
-                    overridePendingTransition(0,0)
+                    overridePendingTransition(0, 0)
                     finish()
 
                 }
             }
             return@setOnNavigationItemSelectedListener false
         }
+        userDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (i in snapshot.children) {
+                    val obj = Upload(
+                        i.child("name").toString(),
+                        i.child("email").toString(),
+                        i.child("address").toString(),
+                        i.child("shop_name").toString(),
+                        i.child("imageUrl").toString(),
+                        i.child("category1").toString(),
+                        i.child("upi").toString()
+                    )
+                }
+            }
+
+        })
+
+
     }
 
 }
