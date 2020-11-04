069af835aab7db2b4760828e01f330cf119c65ef
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-11-04 21:36:27 +0530

    changes made now

--- app/src/main/java/com/example/localzes/Cart.kt
+++ app/src/main/java/com/example/localzes/Cart.kt
@@ -4,45 +4,91 @@ import android.accounts.Account
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_cart.*
 
 class Cart : AppCompatActivity() {
+    private lateinit var cartDatabase: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var recyclerCartProduct: RecyclerView
+    private lateinit var userCartAdapter: AdapterCartItem
+    private lateinit var cartProducts: List<UserCartDetails>
+    private lateinit var totalItems:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        val uid = user!!.uid
+        recyclerCartProduct = findViewById(R.id.cart_recycler_view)
+        recyclerCartProduct.layoutManager = LinearLayoutManager(this)
+        cartProducts=ArrayList<UserCartDetails>()
+        totalItems=findViewById(R.id.txtTotalItems)
+        cartDatabase =
+            FirebaseDatabase.getInstance().reference.child("users").child(uid).child("Cart")
+
         bottom_navCart.selectedItemId = R.id.nav_cart
         bottom_navCart.setOnNavigationItemSelectedListener { item ->
 
 
-            when(item.itemId){
+            when (item.itemId) {
 
-                R.id.nav_home->{
-                    startActivity(Intent(this,Home::class.java))
-                    overridePendingTransition(0,0)
+                R.id.nav_home -> {
+                    startActivity(Intent(this, Home::class.java))
+                    overridePendingTransition(0, 0)
                     finish()
 
                 }
-                R.id.nav_search->{
+                R.id.nav_search -> {
 
-                    startActivity(Intent(this,Search::class.java))
-                    overridePendingTransition(0,0)
+                    startActivity(Intent(this, Search::class.java))
+                    overridePendingTransition(0, 0)
                     finish()
 
                 }
-                R.id.nav_cart->{
+                R.id.nav_cart -> {
 
                     return@setOnNavigationItemSelectedListener true
 
                 }
-                R.id.nav_account->{
+                R.id.nav_account -> {
 
                     startActivity(Intent(this, Accounts::class.java))
-                    overridePendingTransition(0,0)
+                    overridePendingTransition(0, 0)
                     finish()
 
                 }
             }
             return@setOnNavigationItemSelectedListener true
         }
+        cartDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (i in snapshot.children) {
+                    val obj = UserCartDetails(
+                        i.child("orderBy").value.toString(),
+                        i.child("productTitle").value.toString(),
+                        i.child("priceEach").value.toString(),
+                        i.child("finalPrice").value.toString(),
+                        i.child("finalQuantity").value.toString(),
+                        i.child("orderTo").value.toString(),
+                        i.child("productImageUrl").value.toString(),
+                        i.child("sellingPrice").value.toString()
+                    )
+                    (cartProducts as ArrayList<UserCartDetails>).add(obj)
+                }
+                userCartAdapter= AdapterCartItem(this@Cart,cartProducts)
+                recyclerCartProduct.adapter=userCartAdapter
+                totalItems.text="Total Item :- ${snapshot.childrenCount}"
+            }
+
+        })
     }
 }
