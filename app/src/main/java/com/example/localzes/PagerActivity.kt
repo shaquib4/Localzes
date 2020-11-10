787977004a1f69ef5a4da7418b37d346801dcac5
ratnakar1999 <www.ratnakar1999@gmail.com>
2020-11-10 11:45:21 +0530

    fst changes RAtnakar

---
+++ app/src/main/java/com/example/localzes/PagerActivity.kt
@@ -0,0 +1,45 @@
+package com.example.localzes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.view_pager.*
+
+class PagerActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_pager)
+
+        val adapter = MyViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(SHOP() , "SHOP")
+        adapter.addFragment(ITEM() , "ITEM")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+    }
+
+    class MyViewPagerAdapter (manager: FragmentManager) : FragmentPagerAdapter(manager) {
+
+        private val fragmentList : MutableList<Fragment> = ArrayList()
+        private val tittleList : MutableList<String> = ArrayList()
+
+        override fun getItem(position: Int): Fragment {
+         return fragmentList[position]
+        }
+        override fun getCount(): Int {
+            return fragmentList.size
+        }
+
+        fun addFragment(fragment: Fragment, tittle:String){
+             fragmentList.add(fragment)
+            tittleList.add(tittle)
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return tittleList[position]
+        }
+
+    }
+
+}
