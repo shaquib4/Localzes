cc8c47a99aeb8e265cd53cf054f51056d5093c48
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-12-20 19:15:54 +0530

    changes made now

--- app/src/main/java/com/example/localzes/UpdateShopDetailActivity.kt
+++ app/src/main/java/com/example/localzes/UpdateShopDetailActivity.kt
@@ -207,7 +207,7 @@ class UpdateShopDetailActivity : AppCompatActivity() {
     private fun launchCropImage(uri: Uri) {
         CropImage.activity(uri)
             .setGuidelines(CropImageView.Guidelines.ON)
-            .setAspectRatio(16, 5)
+            .setAspectRatio(16, 11)
             .setCropShape(CropImageView.CropShape.RECTANGLE)
             .start(this)
     }
