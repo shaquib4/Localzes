8351d3b02da9345b23fd847a63ad1e2905805858
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-12-20 18:43:40 +0530

    changes made now

---
+++ app/src/main/java/com/example/localzes/ShopBanner.kt
@@ -0,0 +1,11 @@
+package com.example.localzes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopBanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_banner)
+    }
+}
