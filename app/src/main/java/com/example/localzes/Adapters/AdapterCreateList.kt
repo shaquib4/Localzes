5a1bbbaf1bf9a68792638815581e64ccf471c31a
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-12-27 14:38:12 +0530

    changes made now

--- app/src/main/java/com/example/localzes/Adapters/AdapterCreateList.kt
+++ app/src/main/java/com/example/localzes/Adapters/AdapterCreateList.kt
@@ -1,4 +1,36 @@
 package com.example.localzes.Adapters
 
-class AdapterCreateList {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import com.example.localzes.Modals.ModelList
+import com.example.localzes.R
+
+class AdapterCreateList(val context: Context, val item_List: List<ModelList>) :
+    RecyclerView.Adapter<AdapterCreateList.HolderCreateList>() {
+    class HolderCreateList(view: View) : RecyclerView.ViewHolder(view) {
+        val itemName: EditText = view.findViewById(R.id.Itemname)
+        val itemQuantity: EditText = view.findViewById(R.id.Item_Quan)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderCreateList {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_single_row, parent, false)
+        return HolderCreateList(view)
+    }
+
+    override fun getItemCount(): Int {
+        return item_List.size
+    }
+
+    override fun onBindViewHolder(holder: HolderCreateList, position: Int) {
+        val itemList = item_List[position]
+        holder.itemName.setText(itemList.itemId)
+        holder.itemQuantity.setText(itemList.itemQuantity)
+    }
+
 }
---
+++ app/src/main/java/com/example/localzes/Modals/ModelList.kt
@@ -0,0 +1,5 @@
+package com.example.localzes.Modals
+
+class ModelList(val itemId: String, val itemName: String, val itemQuantity: String) {
+    constructor() : this("", "", "")
+}
