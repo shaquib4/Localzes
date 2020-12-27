61d867c2ecf8c6e760000cd213b34270842c1329
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-12-27 17:59:16 +0530

    changes made now

--- app/src/main/java/com/example/localzes/Adapters/AdapterCreateList.kt
+++ app/src/main/java/com/example/localzes/Adapters/AdapterCreateList.kt
@@ -1,19 +1,26 @@
 package com.example.localzes.Adapters
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.localzes.CatalogueActivity
 import com.example.localzes.Modals.ModelList
 import com.example.localzes.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class AdapterCreateList(val context: Context, val item_List: List<ModelList>) :
     RecyclerView.Adapter<AdapterCreateList.HolderCreateList>() {
     class HolderCreateList(view: View) : RecyclerView.ViewHolder(view) {
         val itemName: EditText = view.findViewById(R.id.Itemname)
         val itemQuantity: EditText = view.findViewById(R.id.Item_Quan)
+        val itemRemove: ImageView = view.findViewById(R.id.itemRemove)
 
     }
 
@@ -31,6 +38,85 @@ class AdapterCreateList(val context: Context, val item_List: List<ModelList>) :
         val itemList = item_List[position]
         holder.itemName.setText(itemList.itemId)
         holder.itemQuantity.setText(itemList.itemQuantity)
+        holder.itemName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                saveToDatabase1(s.toString(), itemList.itemId)
+            }
+
+        })
+        holder.itemQuantity.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                saveToDatabase2(s.toString(), itemList.itemId)
+
+            }
+
+        })
+        holder.itemRemove.setOnClickListener {
+            deleteListItem(position)
+        }
+    }
+
+    private fun deleteListItem(position: Int) {
+        val uAuth = FirebaseAuth.getInstance()
+        val user = uAuth.currentUser
+        val uid = user!!.uid
+        val id = item_List[position].itemId
+        val databaseReference: DatabaseReference =
+            FirebaseDatabase.getInstance().reference.child("user").child(uid).child("OrderList")
+        databaseReference.orderByChild("itemId").equalTo(id)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (i in snapshot.children) {
+                        i.ref.removeValue()
+                    }
+                    (context as CatalogueActivity).recreate()
+                }
+            })
+    }
+
+    private fun saveToDatabase2(s: String, itemId: String) {
+        val uAuth = FirebaseAuth.getInstance()
+        val user = uAuth.currentUser
+        val uid = user!!.uid
+        val databaseReference: DatabaseReference =
+            FirebaseDatabase.getInstance().reference.child("user").child(uid).child("OrderList")
+                .child(itemId)
+        val headers = HashMap<String, Any>()
+        headers["itemQuantity"] = s
+        databaseReference.updateChildren(headers)
+    }
+
+
+    private fun saveToDatabase1(s: String, itemId: String) {
+        val uAuth = FirebaseAuth.getInstance()
+        val user = uAuth.currentUser
+        val uid = user!!.uid
+        val databaseReference: DatabaseReference =
+            FirebaseDatabase.getInstance().reference.child("user").child(uid).child("OrderList")
+                .child(itemId)
+        val headers = HashMap<String, Any>()
+        headers["itemName"] = s
+        databaseReference.updateChildren(headers)
     }
 
 }
--- app/src/main/java/com/example/localzes/CreateList.kt
+++ app/src/main/java/com/example/localzes/CreateList.kt
@@ -2,9 +2,10 @@ package com.example.localzes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.localzes.Modals.ModelList
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_create_list.*
 
 class CreateList : AppCompatActivity() {
@@ -13,12 +14,49 @@ class CreateList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_list)
-        auth= FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
         val user = auth.currentUser
         val uid = user!!.uid
+        userDatabase = FirebaseDatabase.getInstance().reference.child("users").child(uid)
+            .child("OrderList")
 
         btnAdd.setOnClickListener {
-            userDatabase=FirebaseDatabase.getInstance().reference.child("users").child(uid)
+            val timestamp = System.currentTimeMillis().toString()
+            val headers = HashMap<String, String>()
+            headers["itemId"] = timestamp
+            headers["itemName"] = ""
+            headers["itemQuantity"] = ""
+            userDatabase.child(timestamp).setValue(headers)
         }
+        userDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (i in snapshot.children) {
+                    if (!(i.child("itemId").value.toString() == "" && i.child("itemName").value.toString() == "")) {
+                        btnCn.setOnClickListener {
+                            val obj = ModelList(
+                                i.child("itemId").value.toString(),
+                                i.child("itemName").value.toString(),
+                                i.child("itemQuantity").value.toString()
+                            )
+                            val shopDatabaseReference=FirebaseDatabase.getInstance().reference
+
+                        }
+                    } else if (i.child("itemId").value.toString() == "" && i.child("itemName").value.toString() == "") {
+                        btnCn.setOnClickListener {
+                            Toast.makeText(
+                                this@CreateList,
+                                "Some fields are empty",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        break
+                    }
+                }
+            }
+        })
     }
 }
