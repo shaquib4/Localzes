8986ce32e329a6a8e9b266926314b2c27c142968
Ekagra Agrawal <agrawalpallavi1981@gmail.com>
2020-12-30 18:52:54 +0530

    changes made now

--- app/src/main/java/com/example/localzes/OrderOutForDeliveryActivity.kt
+++ app/src/main/java/com/example/localzes/OrderOutForDeliveryActivity.kt
@@ -8,11 +8,14 @@ import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.localzes.Adapters.AdapterListOrder
 import com.example.localzes.Adapters.AdapterSellerOrders
+import com.example.localzes.Modals.ModalSellerOrderList
 import com.example.localzes.Modals.ModelOrderDetails
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_order_out_for_delivery.*
+import kotlinx.android.synthetic.main.activity_seller_orders.*
 import util.ConnectionManager
 
 class OrderOutForDeliveryActivity : AppCompatActivity() {
@@ -23,6 +26,8 @@ class OrderOutForDeliveryActivity : AppCompatActivity() {
     private lateinit var adapterOutForDelivery: AdapterSellerOrders
     private lateinit var backOutForDelivery: ImageView
     private lateinit var relativeOutForDelivery: RelativeLayout
+    private lateinit var adapterOutForDeliveryList: AdapterListOrder
+    private lateinit var listOrders: List<ModalSellerOrderList>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_out_for_delivery)
@@ -33,15 +38,41 @@ class OrderOutForDeliveryActivity : AppCompatActivity() {
             this.recreate()
         }
         ordersOutForDeliveryList = ArrayList<ModelOrderDetails>()
+        listOrders = ArrayList<ModalSellerOrderList>()
         recyclerOutForDelivery = findViewById(R.id.recyclerOutForDelivery)
         backOutForDelivery = findViewById(R.id.imgBackOutForDelivery)
         relativeOutForDelivery = findViewById(R.id.rl_Out_For_delivery)
         recyclerOutForDelivery.layoutManager = LinearLayoutManager(this)
         orderDatabaseReference = FirebaseDatabase.getInstance().reference.child("seller").child(uid)
-        if (ConnectionManager().checkConnectivity(this)){
-            rl_DeliveryOrder.visibility=View.VISIBLE
-            rl_retryDeliveryOrder.visibility=View.GONE
-            orderDatabaseReference.child("Orders").orderByChild("orderStatus")
+        rl_listOut.setOnClickListener {
+            listOutForDeliveryOrders()
+        }
+        rl_cartOut.setOnClickListener {
+            cartOutForDeliveryOrders()
+        }
+        if (ConnectionManager().checkConnectivity(this)) {
+            rl_DeliveryOrder.visibility = View.VISIBLE
+            rl_retryDeliveryOrder.visibility = View.GONE
+
+        } else {
+            rl_DeliveryOrder.visibility = View.GONE
+            rl_retryDeliveryOrder.visibility = View.VISIBLE
+        }
+        backOutForDelivery.setOnClickListener {
+            val intent = Intent(this, Home_seller::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun cartOutForDeliveryOrders() {
+        txtCartOut.setTextColor(resources.getColor(R.color.colorPrimary))
+        cartOutNo.setTextColor(resources.getColor(R.color.colorPrimary))
+        txtlistOut.setTextColor(resources.getColor(R.color.black))
+        listOutNo.setTextColor(resources.getColor(R.color.black))
+        val user = orderAuth.currentUser
+        val uid = user!!.uid
+        orderDatabaseReference.child("Orders").orderByChild("orderStatus")
             .equalTo("Out For Delivery").addValueEventListener(object : ValueEventListener {
                 override fun onCancelled(error: DatabaseError) {
 
@@ -81,15 +112,59 @@ class OrderOutForDeliveryActivity : AppCompatActivity() {
                         }
                     }
                 }
-            })}else{
-            rl_DeliveryOrder.visibility=View.GONE
-            rl_retryDeliveryOrder.visibility=View.VISIBLE
-        }
-        backOutForDelivery.setOnClickListener {
-            val intent = Intent(this, Home_seller::class.java)
-            startActivity(intent)
-            finish()
-        }
+            })
+    }
+
+    private fun listOutForDeliveryOrders() {
+        txtCartOut.setTextColor(resources.getColor(R.color.black))
+        cartOutNo.setTextColor(resources.getColor(R.color.black))
+        txtlistOut.setTextColor(resources.getColor(R.color.colorPrimary))
+        listOutNo.setTextColor(resources.getColor(R.color.colorPrimary))
+        val user = orderAuth.currentUser
+        val uid = user!!.uid
+        orderDatabaseReference.child("OrdersLists")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    (listOrders as ArrayList<ModalSellerOrderList>).clear()
+                    for (i in snapshot.children) {
+                        val obj = ModalSellerOrderList(
+                            i.child("orderId").value.toString(),
+                            i.child("orderTime").value.toString(),
+                            i.child("orderStatus").value.toString(),
+                            i.child("orderCost").value.toString(),
+                            i.child("orderBy").value.toString(),
+                            i.child("orderTo").value.toString(),
+                            i.child("deliveryAddress").value.toString(),
+                            i.child("totalItems").value.toString(),
+                            i.child("listStatus").value.toString(),
+                            i.child("orderByName").value.toString(),
+                            i.child("orderByMobile").value.toString()
+                        )
+                        if (i.child("orderStatus").value.toString() == "Out For Delivery") {
+                            (listOrders as ArrayList<ModalSellerOrderList>).add(obj)
+                        }
+                    }
+                    if (listOrders.isEmpty()) {
+                        recyclerOutForDelivery.visibility = View.GONE
+                    } else {
+                        relativeOutForDelivery.visibility = View.GONE
+                        recyclerOutForDelivery.visibility = View.VISIBLE
+                        adapterOutForDeliveryList =
+                            AdapterListOrder(this@OrderOutForDeliveryActivity, listOrders)
+                    }
+                    recyclerOutForDelivery.adapter = adapterOutForDeliveryList
+                }
+            })
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cartOutForDeliveryOrders()
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/example/localzes/PastOrdersActivity.kt
+++ app/src/main/java/com/example/localzes/PastOrdersActivity.kt
@@ -8,7 +8,10 @@ import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.localzes.Adapters.AdapterListOrder
+import com.example.localzes.Adapters.AdapterUserListOrderHistory
 import com.example.localzes.Adapters.AdapterUserOrderHistory
+import com.example.localzes.Modals.ModalSellerOrderList
 import com.example.localzes.Modals.ModelOrderDetails
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
@@ -22,13 +25,15 @@ class PastOrdersActivity : AppCompatActivity() {
     private lateinit var pastOrderHistoryDatabase: DatabaseReference
     private lateinit var userAuth: FirebaseAuth
     private lateinit var mOrderedItem: List<ModelOrderDetails>
-    private lateinit var relativePast:RelativeLayout
-    private lateinit var imgBackPast:ImageView
+    private lateinit var mOrderedItemList: List<ModalSellerOrderList>
+    private lateinit var pastOrderListHistoryAdapter: AdapterUserListOrderHistory
+    private lateinit var relativePast: RelativeLayout
+    private lateinit var imgBackPast: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_past_orders)
-        relativePast=findViewById(R.id.rl_Past_orders)
-        imgBackPast=findViewById(R.id.imgBackPast)
+        relativePast = findViewById(R.id.rl_Past_orders)
+        imgBackPast = findViewById(R.id.imgBackPast)
         userAuth = FirebaseAuth.getInstance()
         val user = userAuth.currentUser
         val uid = user!!.uid
@@ -36,66 +41,133 @@ class PastOrdersActivity : AppCompatActivity() {
             this.recreate()
         }
         mOrderedItem = ArrayList<ModelOrderDetails>()
+        mOrderedItemList = ArrayList<ModalSellerOrderList>()
         pastOrderHistoryDatabase =
-            FirebaseDatabase.getInstance().reference.child("users").child(uid).child("MyOrders")
+            FirebaseDatabase.getInstance().reference.child("users").child(uid)
         recyclerOrderDetails = findViewById(R.id.recyclerPastOrders)
         recyclerOrderDetails.layoutManager = LinearLayoutManager(this)
-        if (ConnectionManager().checkConnectivity(this)){
-            rl_pastOrders.visibility=View.VISIBLE
-            rl_retryPastOrders.visibility=View.GONE
-            pastOrderHistoryDatabase.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                (mOrderedItem as ArrayList<ModelOrderDetails>).clear()
-                for (i in snapshot.children) {
-                    val obj = ModelOrderDetails(
-                        i.child("orderId").value.toString(),
-                        i.child("orderTime").value.toString(),
-                        i.child("orderStatus").value.toString(),
-                        i.child("orderCost").value.toString(),
-                        i.child("orderBy").value.toString(),
-                        i.child("orderTo").value.toString(),
-                        i.child("orderQuantity").value.toString(),
-                        i.child("deliveryAddress").value.toString(),
-                        i.child("paymentMode").value.toString(),
-                        i.child("orderByName").value.toString(),
-                        i.child("orderByMobile").value.toString()
-                    )
-                    if (i.child("orderStatus").value.toString() == "Completed") {
-                        (mOrderedItem as ArrayList<ModelOrderDetails>).add(obj)
-                    }
-
-                }
-                if(mOrderedItem.isEmpty()){
-                    recyclerOrderDetails.visibility= View.GONE
-                } else{
-                    relativePast.visibility=View.GONE
-                    recyclerOrderDetails.visibility=View.VISIBLE
-                    userPastOrderAdapter =
-                        AdapterUserOrderHistory(
-                            this@PastOrdersActivity,
-                            mOrderedItem
-                        )
-                    recyclerOrderDetails.adapter = userPastOrderAdapter
-                }
-
-            }
-        })}else{
-            rl_pastOrders.visibility=View.GONE
-            rl_retryPastOrders.visibility=View.VISIBLE
+        rl_cartPast.setOnClickListener {
+            cartPastOrders()
+        }
+        rl_listPast.setOnClickListener {
+            listPastOrders()
+        }
+        if (ConnectionManager().checkConnectivity(this)) {
+            rl_pastOrders.visibility = View.VISIBLE
+            rl_retryPastOrders.visibility = View.GONE
+        } else {
+            rl_pastOrders.visibility = View.GONE
+            rl_retryPastOrders.visibility = View.VISIBLE
         }
         imgBackPast.setOnClickListener {
-            val intent=Intent(this,Accounts::class.java)
+            val intent = Intent(this, Accounts::class.java)
             startActivity(intent)
             finish()
         }
     }
 
+    private fun listPastOrders() {
+        txtCartPast.setTextColor(resources.getColor(R.color.black))
+        cartPastNo.setTextColor(resources.getColor(R.color.black))
+        txtlistPast.setTextColor(resources.getColor(R.color.colorPrimary))
+        listPastNo.setTextColor(resources.getColor(R.color.colorPrimary))
+        pastOrderHistoryDatabase.child("OrdersLists")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    (mOrderedItemList as ArrayList<ModalSellerOrderList>).clear()
+                    for (i in snapshot.children) {
+                        val obj = ModalSellerOrderList(
+                            i.child("orderId").value.toString(),
+                            i.child("orderTime").value.toString(),
+                            i.child("orderStatus").value.toString(),
+                            i.child("orderCost").value.toString(),
+                            i.child("orderBy").value.toString(),
+                            i.child("orderTo").value.toString(),
+                            i.child("deliveryAddress").value.toString(),
+                            i.child("totalItems").value.toString(),
+                            i.child("listStatus").value.toString(),
+                            i.child("orderByName").value.toString(),
+                            i.child("orderByMobile").value.toString()
+                        )
+                        if (i.child("orderStatus").value.toString() == "Completed") {
+                            (mOrderedItemList as ArrayList<ModalSellerOrderList>).add(obj)
+                        }
+                    }
+                    if (mOrderedItemList.isEmpty()) {
+                        recyclerOrderDetails.visibility = View.GONE
+                    } else {
+                        relativePast.visibility = View.GONE
+                        recyclerOrderDetails.visibility = View.VISIBLE
+                        pastOrderListHistoryAdapter =
+                            AdapterUserListOrderHistory(this@PastOrdersActivity, mOrderedItemList)
+                        recyclerOrderDetails.adapter = pastOrderListHistoryAdapter
+                    }
+                }
+            })
+
+    }
+
+    private fun cartPastOrders() {
+        txtCartPast.setTextColor(resources.getColor(R.color.colorPrimary))
+        cartPastNo.setTextColor(resources.getColor(R.color.colorPrimary))
+        txtlistPast.setTextColor(resources.getColor(R.color.black))
+        listPastNo.setTextColor(resources.getColor(R.color.black))
+        pastOrderHistoryDatabase.child("MyOrders")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    (mOrderedItem as ArrayList<ModelOrderDetails>).clear()
+                    for (i in snapshot.children) {
+                        val obj = ModelOrderDetails(
+                            i.child("orderId").value.toString(),
+                            i.child("orderTime").value.toString(),
+                            i.child("orderStatus").value.toString(),
+                            i.child("orderCost").value.toString(),
+                            i.child("orderBy").value.toString(),
+                            i.child("orderTo").value.toString(),
+                            i.child("orderQuantity").value.toString(),
+                            i.child("deliveryAddress").value.toString(),
+                            i.child("paymentMode").value.toString(),
+                            i.child("orderByName").value.toString(),
+                            i.child("orderByMobile").value.toString()
+                        )
+                        if (i.child("orderStatus").value.toString() == "Completed") {
+                            (mOrderedItem as ArrayList<ModelOrderDetails>).add(obj)
+                        }
+
+                    }
+                    if (mOrderedItem.isEmpty()) {
+                        recyclerOrderDetails.visibility = View.GONE
+                    } else {
+                        relativePast.visibility = View.GONE
+                        recyclerOrderDetails.visibility = View.VISIBLE
+                        userPastOrderAdapter =
+                            AdapterUserOrderHistory(
+                                this@PastOrdersActivity,
+                                mOrderedItem
+                            )
+                        recyclerOrderDetails.adapter = userPastOrderAdapter
+                    }
+
+                }
+            })
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cartPastOrders()
+    }
+
     override fun onBackPressed() {
-        val intent=Intent(this,Accounts::class.java)
+        val intent = Intent(this, Accounts::class.java)
         startActivity(intent)
         finish()
     }
