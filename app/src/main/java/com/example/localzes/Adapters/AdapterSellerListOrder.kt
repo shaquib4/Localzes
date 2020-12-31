9dac85bb16852a46a8f9c968c67756f1a64589e0
Shaquib <mohammadshaquib4@gmail.com>
2020-12-31 21:28:05 +0530

    lion

--- app/src/main/java/com/example/localzes/Adapters/AdapterSellerListOrder.kt
+++ app/src/main/java/com/example/localzes/Adapters/AdapterSellerListOrder.kt
@@ -26,7 +26,7 @@ class AdapterSellerListOrder(
     class HolderSellerListOrder(view: View) : RecyclerView.ViewHolder(view) {
         val txtSNo: TextView = view.findViewById(R.id.txtNo)
         val txtItem_Name: TextView = view.findViewById(R.id.txtItem_Name)
-        val txtQuan: TextView = view.findViewById(R.id.txtQuan)
+
         val edtPrice: EditText = view.findViewById(R.id.edtPrice)
         val itemRemove: ImageView = view.findViewById(R.id.itemRemove)
         val itemRevive: ImageView = view.findViewById(R.id.itemRevive)
@@ -47,8 +47,8 @@ class AdapterSellerListOrder(
     override fun onBindViewHolder(holder: HolderSellerListOrder, position: Int) {
         val sellerOrderList = seller_order_list[position]
         holder.txtSNo.text = (position + 1).toString() + "."
-        holder.txtItem_Name.text=sellerOrderList.itemName
-        holder.txtQuan.text=sellerOrderList.itemQuantity
+        holder.txtItem_Name.text="${sellerOrderList.itemName}X${sellerOrderList.itemQuantity}"
+
         val uAuths = FirebaseAuth.getInstance()
         val users = uAuths.currentUser
         val uids = users!!.uid
