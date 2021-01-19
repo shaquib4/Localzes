40476c66f0adacfc92ec8108cd5eb097da76402c
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-01-19 18:58:50 +0000

    new commit

--- app/src/main/java/com/example/localzes/Adapters/AdapterManageAddress.kt
+++ app/src/main/java/com/example/localzes/Adapters/AdapterManageAddress.kt
@@ -113,7 +113,7 @@ class AdapterManageAddress(
         val databaseReference: DatabaseReference =
             FirebaseDatabase.getInstance().reference.child("users").child(uid).child("address")
         databaseReference.orderByChild("id").equalTo(id)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
+            .addValueEventListener(object : ValueEventListener {
                 override fun onCancelled(error: DatabaseError) {
 
                 }
