35e7fe4fddf2013d5c761d96da4e380d656d3d27
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-01-19 13:23:18 +0000

    new commit

--- app/src/main/java/com/example/localzes/ShopBanner.kt
+++ app/src/main/java/com/example/localzes/ShopBanner.kt
@@ -9,7 +9,6 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.pdf.PdfDocument
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
