77d2961d353337bf15bb271bb0bc035eca1a6f14
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-01-22 21:16:39 +0530

    fom Shaquib

--- app/src/main/java/com/localzes/android/cardBanners.kt
+++ app/src/main/java/com/localzes/android/cardBanners.kt
@@ -3,13 +3,22 @@ package com.localzes.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_card_banners.*
 
 class cardBanners : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_banners)
-
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        val uid = user!!.uid
         bottom_navOrders_seller.selectedItemId = R.id.nav_order_seller
         bottom_navOrders_seller.setOnNavigationItemSelectedListener {
             when (it.itemId) {
@@ -52,7 +61,70 @@ class cardBanners : AppCompatActivity() {
 
             return@setOnNavigationItemSelectedListener false
         }
-        CardBanners()
+      val  cardDatabaseRef =
+            FirebaseDatabase.getInstance().reference.child("seller")
+        cardDatabaseRef.child(uid)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (!(snapshot.child("staffOfShop")
+                            .exists()) || snapshot.child("staffOfShop").value.toString() == ""
+                    ) {
+                        CardBanners()
+                    } else {
+                        val uidOfShop = snapshot.value.toString()
+                        if (snapshot.child("StaffOf").child(uidOfShop).exists()) {
+                            val databaseReference =
+                                FirebaseDatabase.getInstance().reference.child("seller")
+                                    .child(uidOfShop).child("MyStaff").child(uid)
+                            databaseReference.addValueEventListener(object : ValueEventListener {
+                                override fun onCancelled(error: DatabaseError) {
+
+                                }
+
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    val access = snapshot.child("access").value.toString()
+                                    when (access) {
+                                        "Total Access" -> {
+                                            CardBanners()
+                                        }
+                                        "Order Access" -> {
+                                            rl_accessCard.visibility= View.GONE
+                                            cardScroll.visibility=View.VISIBLE
+                                        }
+                                        "Delivery Access" -> {
+                                            rl_accessCard.visibility= View.GONE
+                                            cardScroll.visibility=View.VISIBLE
+                                        }
+                                        "Catalogue Access(Product)" -> {
+                                            rl_accessCard.visibility= View.GONE
+                                            cardScroll.visibility=View.VISIBLE
+                                        }
+                                        "Boost Your Shop Access" -> {
+                                            carStaff.visibility=View.GONE
+                                            CardBanners()
+                                        }
+                                        "(Orders + Catalogue)Access" -> {
+                                            rl_accessCard.visibility= View.GONE
+                                            cardScroll.visibility=View.VISIBLE
+                                        }
+                                        "(Order + Boost Your Shop)Access" -> {
+                                            carStaff.visibility=View.GONE
+                                            CardBanners()
+                                        }
+                                    }
+                                }
+
+                            })
+                        }
+                    }
+
+                }
+
+            })
 
     }
     private fun CardBanners(){
