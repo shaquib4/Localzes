930eb99c55d7bad2391cd666633606b485b5f44e
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-01-23 19:39:20 +0530

    fom Shaquib

---
+++ app/src/main/java/com/localzes/android/SellerAcceptedRejectedInvitation.kt
@@ -0,0 +1,11 @@
+package com.localzes.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerAcceptedRejectedInvitation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_accepted_rejected_invitation)
+    }
+}
