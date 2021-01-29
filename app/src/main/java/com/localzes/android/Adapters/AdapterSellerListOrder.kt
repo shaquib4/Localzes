fcec8f7975add9bf79a570d5c758d6b2fa8855c4
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-01-29 23:46:23 +0530

    ListScroll

--- app/src/main/java/com/localzes/android/Adapters/AdapterSellerListOrder.kt
+++ app/src/main/java/com/localzes/android/Adapters/AdapterSellerListOrder.kt
@@ -30,7 +30,7 @@ class AdapterSellerListOrder(
     class HolderSellerListOrder(view: View) : RecyclerView.ViewHolder(view) {
         val txtSNo: TextView = view.findViewById(R.id.txtNo)
         val txtItem_Name: TextView = view.findViewById(R.id.txtItem_Name)
-
+        val qun:TextView=view.findViewById(R.id.quantityList)
         val edtPrice: EditText = view.findViewById(R.id.edtPrice)
         val itemRemove: ImageView = view.findViewById(R.id.itemRemove)
         val itemRevive: ImageView = view.findViewById(R.id.itemRevive)
@@ -51,7 +51,8 @@ class AdapterSellerListOrder(
     override fun onBindViewHolder(holder: HolderSellerListOrder, position: Int) {
         val sellerOrderList = seller_order_list[position]
         holder.txtSNo.text = (position + 1).toString() + "."
-        holder.txtItem_Name.text = "${sellerOrderList.itemName}X${sellerOrderList.itemQuantity}"
+        holder.txtItem_Name.text = "${sellerOrderList.itemName}"
+        holder.qun.text="${sellerOrderList.itemQuantity}"
         val mString = "${sellerOrderList.itemName}X${sellerOrderList.itemQuantity}"
         val spannableString = SpannableString(mString)
         val mStrikeThrough = StrikethroughSpan()
@@ -131,7 +132,7 @@ class AdapterSellerListOrder(
 
 
                     } else {
-                        holder.edtPrice.setText(sellerOrderList.itemCost)
+
                         holder.itemRemove.visibility = View.VISIBLE
                         holder.itemRevive.visibility = View.GONE
                         holder.edtPrice.addTextChangedListener(object : TextWatcher {
