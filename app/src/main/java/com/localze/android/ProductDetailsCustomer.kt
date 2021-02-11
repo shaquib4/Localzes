d0cb26fff880c0def91f4912e808bb395ef757f7
ratnakar1999 <www.ratnakar1999@gmail.com>
2021-02-11 20:56:21 +0530

    fst changes RAtnakar

---
+++ app/src/main/java/com/localze/android/ProductDetailsCustomer.kt
@@ -0,0 +1,11 @@
+package com.localze.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetailsCustomer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details_customer)
+    }
+}
