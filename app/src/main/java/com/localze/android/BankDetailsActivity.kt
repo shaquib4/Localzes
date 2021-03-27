3c87e4d5e59b12b622580dce0c56d18e5b56cf41
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-03-27 20:57:55 +0000

    account details added

---
+++ app/src/main/java/com/localze/android/BankDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.localze.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BankDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bank_details)
+    }
+}
