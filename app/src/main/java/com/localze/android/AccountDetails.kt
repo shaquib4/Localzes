670454b8a53a0aa06ba0ebe11af30be6c9e21895
Ekagra Agrawal <ekagraagrawal49@gmail.com>
2021-03-27 21:13:04 +0000

    account details added

--- app/src/main/java/com/localze/android/AccountDetails.kt
+++ app/src/main/java/com/localze/android/AccountDetails.kt
@@ -13,32 +13,35 @@ class AccountDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_details)
-        auth=FirebaseAuth.getInstance()
-        val user=auth.currentUser
-        val uid=user!!.uid
-        databaseReference=FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        val uid = user!!.uid
+        databaseReference = FirebaseDatabase.getInstance().reference
+        addDetails.setOnClickListener {
+            when {
+                edtAccountNumber.text.isEmpty() -> {
+                    edtAccountNumber.error = "Please Enter Account Number"
+                }
+                edtBeneficiaryName.text.isEmpty() -> {
+                    edtBeneficiaryName.error = "Please Enter Name"
+                }
+                edtIFSC.text.isEmpty() -> {
+                    edtIFSC.error = "Please Enter IFSC Code"
+                }
+                else -> {
+                    val headers = HashMap<String, Any>()
+                    val header = HashMap<String, Any>()
+                    headers["Account_no"] = edtAccountNumber.text.toString()
+                    headers["Beneficiary_name"] = edtBeneficiaryName.text.toString()
+                    headers["Account_type"] = spn_type.selectedItem.toString()
+                    headers["IFSC"] = edtIFSC.text.toString()
+                    header["Account_Details"] = "true"
 
-        when{
-            edtAccountNumber.text.isEmpty()->{
-                edtAccountNumber.error="Please Enter Account Number"
+                    databaseReference.child("seller").child(uid).updateChildren(header)
+                    databaseReference.child("customers").child(uid).child("AccountDetails")
+                        .updateChildren(headers)
+                }
             }
-            edtBeneficiaryName.text.isEmpty()->{
-                edtBeneficiaryName.error="Please Enter Name"
-            }
-            edtIFSC.text.isEmpty()->{
-                edtIFSC.error="Please Enter IFSC Code"
-            }else->{
-            val headers=HashMap<String,Any>()
-            val header=HashMap<String,Any>()
-            headers["Account_no"]=edtAccountNumber.text.toString()
-            headers["Beneficiary_name"]=edtBeneficiaryName.text.toString()
-            headers["Account_type"]=spn_type.selectedItem.toString()
-            headers["IFSC"]=edtIFSC.text.toString()
-            header["Account_Details"]="true"
-
-            databaseReference.child("seller").child(uid).updateChildren(header)
-            databaseReference.child("customers").child(uid).child("AccountDetails").updateChildren(headers)
-         }
         }
 
 
