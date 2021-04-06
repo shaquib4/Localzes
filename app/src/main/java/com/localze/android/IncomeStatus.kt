5fa862d9d77315fbc9635b22913f78ddf53ddd59
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-04-06 20:34:44 +0530

    razorpay

---
+++ app/src/main/java/com/localze/android/IncomeStatus.kt
@@ -0,0 +1,11 @@
+package com.localze.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IncomeStatus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_income_status)
+    }
+}
