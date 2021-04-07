6dd321ccfe93b298f4c599df958d2a604994ea14
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-04-07 19:49:54 +0530

    rax

--- app/src/main/java/com/localze/android/IncomeType.kt
+++ app/src/main/java/com/localze/android/IncomeType.kt
@@ -1,11 +1,32 @@
 package com.localze.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_income_type.*
 
 class IncomeType : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_income_type)
+
+        cardCOD.setOnClickListener {
+            val intent= Intent(this,IncomeStatus::class.java)
+            intent.putExtra("IncomeType","COD")
+            startActivity(intent)
+            finish()
+        }
+        cardPAYTM.setOnClickListener {
+            val intent= Intent(this,IncomeStatus::class.java)
+            intent.putExtra("IncomeType","PAYTM")
+            startActivity(intent)
+            finish()
+        }
+        cardRAZORPAY.setOnClickListener {
+            val intent= Intent(this,IncomeStatus::class.java)
+            intent.putExtra("IncomeType","RAZORPAY")
+            startActivity(intent)
+            finish()
+        }
     }
 }
