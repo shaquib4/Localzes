caddedaebb103ee0ce7c80d2efaa7bacf932f38b
Mohd Shaquib Khan <mohammadshaquib4@gmail.com>
2021-04-07 19:43:22 +0530

    razorpay

---
+++ app/src/main/java/com/localze/android/IncomeType.kt
@@ -0,0 +1,11 @@
+package com.localze.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IncomeType : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_income_type)
+    }
+}
